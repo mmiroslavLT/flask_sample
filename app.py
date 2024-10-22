2c66f3636582d76caba9af1da571f34edc45270a
Miroslav Milivojevic <miroslav@liquidtext.net>
2024-10-22 22:20:13 +0200

    fetch second page

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 # app.py
 
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
+import os
 
 app = Flask(__name__)
 
-app.config['SERVER_NAME'] = 'mmiroslav.pythonanywhere.com'
+if os.environ.get('FLASK_ENV') == 'production':
+    app.config['SERVER_NAME'] = 'mmiroslav.pythonanywhere.com'
+else:
+    app.config['SERVER_NAME'] = '127.0.0.1:5000'
 
 @app.route('/')
-def hello():
-    return 'Hello world!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/fetch-page')
+def fetch_page():
+    headers = dict(request.headers)
+
+    title = "Welcome to the second page"
+
+    return render_template('second_page.html',
+                           title=title,
+                           headers=headers)
 
 if __name__ == '__main__':
     app.run(debug=True)
